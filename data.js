bfe04e25b932f338d836364b702d78b27ecaae8e
AndreaL <alafertteo@gmail.com>
2019-01-03 11:24:16 -0300

    data.js agregados espacios en tipos y debilidades, index.html agregado id a botones, style.css cambio de fuente

--- data.js
+++ data.js
@@ -5,14 +5,13 @@ fetch(pokemones)
   .then(data => {
     console.log(JSON.stringify(data));
     let dataPokedex = data.pokemon;
-    //let groupPokemon;
     dataPokedex.forEach(function(element) { 
       return groupPokemon += `<div class="card">
            <img class="card-img-top" src="` + element.img +`" alt="Card image cap">
           <div class="card-body">
             <h5 class="card-title">` + element.name + ` #` + element.id + `</h5>
-              <p class="card-text"> Type: ` + element.type + `</p>
-              <p class="card-text"> Weaknesses: ` + element.weaknesses + `</p>
+              <p class="card-text"> Type: ` + element.type.join(', ') + `</p>
+              <p class="card-text"> Weaknesses: ` + element.weaknesses.join(', ') + `</p>
           </div>
           <div class="card-footer">
             <small class="text-muted"> Hatches from egg: ` + element.egg + `</small>
@@ -20,8 +19,15 @@ fetch(pokemones)
         </div>`
     });
     document.getElementById('root').innerHTML = groupPokemon; 
+  })
+  .catch(error => {
+    console.error('Error:', error)
   });
+ 
 
+window.filterData = (data, condition) => {
+data.sort
+}
 
 /* esta es una función de ejemplo
 / puedes ver como agregamos la función a nuestro objeto global window
@@ -33,10 +39,15 @@ const example = () => {
 window.example = example;
 */
 
-/* filterData(data, condition)
+/* filterData(data, condition) recibiría la data, y nos retornaría aquellos datos que 
+sí cumplan con la condición
 
-sortData(data, sortBy, sortOrder)
+sortData(data, sortBy, sortOrder) El primer parámetro, data, nos entrega los datos. 
+El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data 
+se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere 
+ordenar de manera ascendente o descendente.
 
-computeStats(data)
+computeStats(data) nos permitirá hacer cálculos estadísticos básicos para ser
+ mostrados de acuerdo a la data proporcionada.
 */
 

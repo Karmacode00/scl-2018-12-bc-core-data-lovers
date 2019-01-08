28f4eb75f9fcdba4697ba402d575f4398905e46f
AndreaL <alafertteo@gmail.com>
2019-01-08 12:56:38 -0300

    main.js función de filtrado por tipo de pokemon

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ window.filterType = (data, condition) =>{
   console.log(condition); console.log(typeof condition);
   const filteredType = data.filter(element => {
     console.log(element.type);
-    return element.type === condition
+    return element.type.includes(condition,0)
   });
   return filteredType;
 };

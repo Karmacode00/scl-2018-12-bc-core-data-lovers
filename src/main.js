ee28e5f90b69a2c19ddf87f4c89d676ff8dfefe2
AndreaL <alafertteo@gmail.com>
2019-01-10 08:28:49 -0300

    data.js y main.js función sort

--- src/main.js
+++ src/main.js
@@ -1,25 +1,60 @@
-window.filterType = (data, condition) =>{
-  //console.log(condition); console.log(typeof condition);
+window.pokemon = {
+filterType: (data, condition) =>{
   const filteredType = data.filter(element => {
-    //console.log(element.type);
     return element.type.includes(condition,0)
   });
   return filteredType;
-};
+},
 
-window.sortData = (data, sortBy, sortOrder) =>{
-  let mapped = data.map(function(el, i) {
-    return { index: i, value: el.toLowerCase() };
-  });
-  
-  // sorting the mapped array containing the reduced values
-  mapped.sort(function(a, b) {
-    if (a.value > b.value) {
-      return 1;
-    }
-    if (a.value < b.value) {
-      return -1;
-    } else {
-    return 0;
-    }
-  })};
+sortData: (data, sortBy, sortOrder) =>{
+  if(sortBy === 'num' && sortOrder === 'des'){
+    const sortedNum = data.sort((a,b) => {
+      if (a.num < b.num) {
+        return 1;
+      }
+      if (a.num > b.num) {
+        return -1;
+      } else {
+      return 0;
+      }
+    })
+    return sortedNum;
+  }else if(sortBy === 'num' && sortOrder === 'asc'){
+    const sortedNum = data.sort((a,b) => {
+      if (a.num > b.num) {
+        return 1;
+      }
+      if (a.num < b.num) {
+        return -1;
+      } else {
+      return 0;
+      }
+    })
+    return sortedNum;
+  }else if(sortBy === 'name' && sortOrder === 'des'){
+    const sortedName = data.sort((a,b) => {
+      if (a.name < b.name) {
+        return 1;
+      }
+      if (a.name > b.name) {
+        return -1;
+      } else {
+      return 0;
+      }
+    })
+    return sortedName;
+  }else if(sortBy === 'name' && sortOrder === 'asc'){
+    const sortedName = data.sort((a,b) => {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      } else {
+      return 0;
+      }
+    })
+    return sortedName;
+  }
+}
+}

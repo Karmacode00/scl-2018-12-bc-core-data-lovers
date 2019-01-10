1156660351a677f0460ad63fbfaadd109be91f2c
AndreaL <alafertteo@gmail.com>
2019-01-10 08:28:49 -0300

    data.js y main.js función sort

--- data.js
+++ data.js
@@ -34,7 +34,7 @@ const selectType = document.getElementById('type');
 
 selectType.addEventListener('change', ()=> {
   let condition = selectType.options[selectType.selectedIndex].value;
-  let filtered = window.filterType(data.pokemon, condition);
+  let filtered = window.pokemon.filterType(data.pokemon, condition);
   let filteredPokemon = '';
   filtered.forEach(element => {
     return filteredPokemon += `<div class="card">
@@ -58,11 +58,12 @@ selectAll.addEventListener('click', ()=>{
   showData(data);
 });
 
-/*const selectAlphabetic = document.getElementById('alphabetic');
+const sortAlphabeticAsc = document.getElementById('alphabetic');
 
-selectAlphabetic.addEventListener('click', ()=>{
-  let sorted = window.sortData(data, sortBy, sortOrder)
-  let sortBy = data.pokemon;
+sortAlphabeticAsc.addEventListener('click', ()=>{
+  let sortBy = 'name';
+  let sortOrder = 'asc';
+  let sorted = window.pokemon.sortData(data.pokemon, sortBy, sortOrder)
   let sortedPokemon = '';
   sorted.forEach(element => {
     return sortedPokemon += `<div class="card">
@@ -79,14 +80,59 @@ selectAlphabetic.addEventListener('click', ()=>{
   });
   document.getElementById('root').innerHTML = sortedPokemon;
 });
-*/
 
-const selectNumeric = document.getElementById('numericDes');
+const sortAlphabeticDes = document.getElementById('reverseAlphabetic');
 
-selectNumeric.addEventListener('click', ()=>{
-  let sorted = window.sortData(data, sortBy, sortOrder)
-  let sortBy = data.pokemon.number;
+sortAlphabeticDes.addEventListener('click', ()=>{
+  let sortBy = 'name';
   let sortOrder = 'des';
+  let sorted = window.pokemon.sortData(data.pokemon, sortBy, sortOrder)
+  let sortedPokemon = '';
+  sorted.forEach(element => {
+    return sortedPokemon += `<div class="card">
+    <img class="card-img-top" src=${element.img} alt="Card image cap">
+   <div class="card-body">
+     <h5 class="card-title">${element.name} #${element.id}</h5>
+       <p class="card-text"> Type: ${element.type.join(', ')}</p>
+       <p class="card-text"> Weaknesses: ${element.weaknesses.join(', ')}</p>
+   </div>
+   <div class="card-footer">
+     <small class="text-muted"> Hatches from egg: ${element.egg}</small>
+   </div>
+ </div>`
+  });
+  document.getElementById('root').innerHTML = sortedPokemon;
+});
+
+const sortNumericAsc = document.getElementById('numericAsc');
+
+sortNumericAsc.addEventListener('click', ()=>{
+  let sortBy = 'num';
+  let sortOrder = 'asc';
+  let sorted = window.pokemon.sortData(data.pokemon, sortBy, sortOrder)
+  let sortedPokemon = '';
+  sorted.forEach(element => {
+    return sortedPokemon += `<div class="card">
+    <img class="card-img-top" src=${element.img} alt="Card image cap">
+   <div class="card-body">
+     <h5 class="card-title">${element.name} #${element.id}</h5>
+       <p class="card-text"> Type: ${element.type.join(', ')}</p>
+       <p class="card-text"> Weaknesses: ${element.weaknesses.join(', ')}</p>
+   </div>
+   <div class="card-footer">
+     <small class="text-muted"> Hatches from egg: ${element.egg}</small>
+   </div>
+ </div>`
+  });
+  document.getElementById('root').innerHTML = sortedPokemon;
+});
+
+const sortNumericDes = document.getElementById('numericDes');
+
+sortNumericDes.addEventListener('click', ()=>{
+  let sortBy = 'num';
+  let sortOrder = 'des';
+  let sorted = window.pokemon.sortData(data.pokemon, sortBy, sortOrder)
   let sortedPokemon = '';
   sorted.forEach(element => {
     return sortedPokemon += `<div class="card">
@@ -107,10 +153,6 @@ selectNumeric.addEventListener('click', ()=>{
 
 
 /*
-sortData(data, sortBy, sortOrder) El primer parámetro, data, nos entrega los datos. 
-El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data 
-se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere 
-ordenar de manera ascendente o descendente.
 
 computeStats(data) nos permitirá hacer cálculos estadísticos básicos para ser
  mostrados de acuerdo a la data proporcionada.
